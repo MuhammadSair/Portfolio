54dd53a3263de47f317213b72a7e0227a3d62c95
MuhammadSair <msair565@gmail.com>
2024-10-24 18:03:24 +0500

    basics-ok

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-document.querySelector('.header-menu-links').addEventListener('click', (event) => {
-    if (event.target.tagName === 'li') {
-      console.log(`You clicked on ${event.target.textContent}`);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerButton = document.getElementById('hamburger-button');
+  const closeButton = document.getElementById('close-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const menuOptions = document.querySelectorAll('.menu-option');
+
+  hamburgerButton.addEventListener('click', function () {
+    console.log('click');
+    
+    mobileMenu.classList.add('show');
   });
-  
+
+  closeButton.addEventListener('click', function () {
+    mobileMenu.classList.remove('show');
+  });
+
+  menuOptions.forEach(function (option) {
+    option.addEventListener('click', function () {
+      mobileMenu.classList.remove('show');
+      const targetId = option.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
